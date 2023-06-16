d5c89185187a320412f96c6a6c9c7cce9c9b6cda
mamadoumouctar.diall <mamadoumouctar.diallo.@cgi.com>
2023-06-16 15:04:02 +0200

    templating

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-navbar',
+    //template: '<h1> Navbar Component </h1>',
+    templateUrl: './navbar.component.html',
+    //styles: [`h1{
+              //  color: yellow;
+              //  font-size: 50px;
+    //}`]
+    styleUrls: ['./navbar.component.css']
+})
+
+
+export  class NavBarComponent {
+
+}
